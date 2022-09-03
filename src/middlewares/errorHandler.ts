01e58695c7540219eb7263adbd5db7a9596d92d4
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-02 22:28:11 -0300

    feat: implement return on data error

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 
 async function errorHandler(error: any, req: Request, res: Response, next: NextFunction) {
+    if(error.type === "error_data") return res.status(422).send({ message: error.message });
+    
+
     res.sendStatus(500);
 }
 
