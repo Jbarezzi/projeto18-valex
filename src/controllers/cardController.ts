30e719486816ca00228bc07ceb813d08221850ab
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-04 13:53:50 -0300

    feat: implement card activation controller

--- src/controllers/cardController.ts
+++ src/controllers/cardController.ts
@@ -1,14 +1,23 @@
 import type {Request, Response} from 'express';
 import type {TransactionTypes} from '../repositories/cardRepository.js';
-import {createNewCard} from '../services/cardService.js';
+import createCardService from '../services/cardService/cardCreation.js';
+import activateCardService from './../services/cardService/cardActivation.js';
 
 async function createCard(req: Request, res: Response) {
   const { employeeId, cardType }: { employeeId: number, cardType: TransactionTypes } = req.body;
   const apiKey: string = res.locals.key;
 
-  await createNewCard(employeeId, cardType, apiKey);
+  await createCardService(employeeId, cardType, apiKey);
 
   res.sendStatus(201);
 }
 
-export { createCard };
+async function activateCard(req: Request, res: Response) {
+  const {cardId, securityCode, password}: {cardId: number, securityCode: string, password: string} = req.body;
+
+  await activateCardService(cardId, securityCode, password);
+
+  res.sendStatus(200);
+}
+
+export { createCard, activateCard };
