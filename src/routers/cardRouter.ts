e13371eefa6417a540b3415ef213bc000c31c5b5
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-04 13:05:56 -0300

    feat: implement validation of schema with joi on card activation

--- src/routers/cardRouter.ts
+++ src/routers/cardRouter.ts
@@ -2,11 +2,12 @@ import {Router} from 'express';
 import {createCard} from '../controllers/cardController.js';
 import joiValidator from '../middlewares/joiValidator.js';
 import verifyApiKey from '../middlewares/verifyApiKey.js';
-import {cardCreationSchema} from '../schemas/cardSchemas.js';
+import {cardActivationSchema, cardCreationSchema} from '../schemas/cardSchemas.js';
 
 
 const cardRouter = Router();
 
 cardRouter.post("/create-card", joiValidator(cardCreationSchema), verifyApiKey, createCard);
+cardRouter.post("/card-activation", joiValidator(cardActivationSchema));
 
 export default cardRouter;
