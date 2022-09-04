41904b4d0e75f1098171654b34f3640adf984e2b
Juan Barezzi <juan.barezzi@gmail.com>
2022-09-04 01:37:39 -0300

    refactor: change error type

--- src/middlewares/verifyApiKey.ts
+++ src/middlewares/verifyApiKey.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from "express";
 function verifyApiKey(req: Request, res: Response, next: NextFunction) {
   const key: string | string[] | undefined = req.headers['x-api-key'];
 
-  if(key === '') throw { type: "error_missing_api_key", message: "Nao foi encontrada a chave de acesso da empresa." };
+  if(key === '') throw { type: "error_unauthorized", message: "Nao foi encontrada a chave de acesso da empresa." };
   res.locals.key = key;
   next();
 }
